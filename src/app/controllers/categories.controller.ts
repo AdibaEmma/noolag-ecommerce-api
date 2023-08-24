16a3895a9adbe8aaad7787c5d6a86e5bda843b76
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-24 01:45:45 +0000

    create category dto

--- src/app/controllers/categories.controller.ts
+++ src/app/controllers/categories.controller.ts
@@ -1,6 +1,11 @@
+import { CategoriesService } from '@app/services';
 import {Controller} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {ApiTags} from '@nestjs/swagger';
 
 @ApiTags('products')
 @Controller()
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  
+}
--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 4000);
   console.log(`Application is running on: ${process.env.PORT}`);
-
 }
 bootstrap();
