73a106a5b276db86aff32c4fb403d2196919052e
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-26 23:08:21 +0000

    update swagger doc

--- src/app/auth/dto/login.dto.ts
+++ src/app/auth/dto/login.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {IsEmail, IsNotEmpty, IsString} from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty()
   @IsEmail({}, {message: 'Please enter a valid email'})
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   email: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   password: string;
 }
--- src/app/auth/dto/signup.dto.ts
+++ src/app/auth/dto/signup.dto.ts
@@ -1,20 +1,37 @@
 import {Match} from '@app/decorators/match.decorator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {IsAlphanumeric, IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength} from 'class-validator';
 
 export class SignupDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   firstName: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   lastName: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   email: string;
 
   @IsAlphanumeric()
+  @ApiPropertyOptional({
+    type: String,
+    description: 'This is an optional property',
+  })
   username: string;
 
   @IsNotEmpty()
@@ -24,6 +41,10 @@ export class SignupDto {
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'password too weak, It should contain at least, 1 Uppercase, 1 Numeric, 1 Lowercase, 1 special characters',
   })
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   password: string;
 
   @IsNotEmpty()
@@ -31,5 +52,9 @@ export class SignupDto {
   @MinLength(6)
   @MaxLength(20)
   @Match('password', {message: 'password does not match'})
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   confirmPassword: string;
 }
