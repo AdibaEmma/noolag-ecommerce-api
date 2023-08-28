47d035ca7ce2d75c0d1ade709c94b00f77743301
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-28 13:42:41 +0000

    update orderItem entity

--- src/app/entities/order-items.entity.ts
+++ src/app/entities/order-items.entity.ts
@@ -1,5 +1,7 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {AutoIncrement, Column, Model, PrimaryKey, Table} from 'sequelize-typescript';
+import {AutoIncrement, BelongsTo, Column, ForeignKey, Model, PrimaryKey, Table} from 'sequelize-typescript';
+import {Product} from './products.entity';
+import {Order} from './orders.entity';
 
 @Table
 export class OrderItem extends Model<OrderItem> {
@@ -8,4 +10,18 @@ export class OrderItem extends Model<OrderItem> {
   @Column
   @ApiProperty({example: 1, description: 'id of the orderItem'})
   id: number;
+
+  @ForeignKey(() => Order)
+  @BelongsTo(() => Order)
+  order: Order;
+
+  @ForeignKey(() => Product)
+  @BelongsTo(() => Product)
+  product: Product;
+
+  @Column
+  quantity: number;
+
+  @Column({type: 'DECIMAL(10, 2)', allowNull: false})
+  subtotal: number;
 }
