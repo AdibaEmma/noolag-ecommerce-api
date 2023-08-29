d3d8b8cb473e82d0aecf833c4346d1d61f4ebd4d
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-29 22:03:22 +0000

    update

--- src/app/dtos/create-payment.dto.ts
+++ src/app/dtos/create-payment.dto.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {PaymentMethod} from '@app/enums';
 import {IsCurrencySymbol, IsPaymentMethod} from '@app/validators';
-import {IsDecimal, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsPositive} from 'class-validator';
+import {IsDecimal, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, ValidateIf, ValidateNested} from 'class-validator';
+import {MomoPaymentDto} from './momo-payment.dto';
+import {CreditCardPaymentDto} from './credit-card-payment.dto';
+import {Type} from 'class-transformer';
 
 export class CreatePaymentDto {
   @IsNotEmpty()
@@ -23,4 +27,14 @@ export class CreatePaymentDto {
   @IsEnum(PaymentMethod)
   @IsPaymentMethod()
   paymentMethod: PaymentMethod;
+
+  @ValidateIf((object, value) => object.paymentMethod === PaymentMethod.Momo)
+  @ValidateNested()
+  @Type(() => MomoPaymentDto)
+  momoPaymentDetails: MomoPaymentDto;
+
+  @ValidateIf((object, value) => object.paymentMethod === PaymentMethod.CreditCard)
+  @ValidateNested()
+  @Type(() => CreditCardPaymentDto)
+  creditCardPaymentDetails: CreditCardPaymentDto;
 }
