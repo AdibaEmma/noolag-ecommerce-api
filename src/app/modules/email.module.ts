bd8298d2c8370ca765f2e3b4c71c221f73319eed
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2023-08-30 17:24:51 +0000

    minor changes

--- src/app/modules/email.module.ts
+++ src/app/modules/email.module.ts
@@ -2,10 +2,11 @@ import {Logger, Module} from '@nestjs/common';
 import {EmailService, UsersService, ValidationService} from '@app/services';
 import {EmailController} from '@app/controllers/email.controller';
 import {DatabaseModule} from './database.module';
+import {rolesProviders, usersProviders} from '@app/providers';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [EmailService, UsersService, ValidationService, Logger],
+  providers: [EmailService, UsersService, ValidationService, Logger, ...usersProviders, ...rolesProviders],
   controllers: [EmailController],
   exports: [EmailService],
 })
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -2,6 +2,7 @@ export * from './database.providers';
 export * from './categories.providers';
 export * from './products.providers';
 export * from './users.providers';
+export * from './roles.provider';
 export * from './orders.providers';
 export * from './orders-items.providers';
 export * from './transactions.provider';
